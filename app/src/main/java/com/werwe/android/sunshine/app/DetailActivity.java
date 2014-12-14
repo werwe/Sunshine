0edae9a32687f73c933f21ec7f7d550848d0884e
wrewe <werwe.me@gmail.com>
2014-12-15 01:59:53 +0900

    인텐트를 가져와 내용을 TextView에 출력하

--- app/src/main/java/com/werwe/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/werwe/android/sunshine/app/DetailActivity.java
@@ -1,5 +1,7 @@
 package com.werwe.android.sunshine.app;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -60,6 +63,14 @@ public class DetailActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String extraText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                TextView detail = (TextView) rootView.findViewById(R.id.detail_text);
+                detail.setText(extraText);
+            }
+
             return rootView;
         }
     }
