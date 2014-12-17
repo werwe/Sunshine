4f4b95437f007a25711a0182bb7f61a47a460277
wrewe <werwe.me@gmail.com>
2014-12-17 11:49:08 +0900

    Map Location 추가

--- app/src/main/java/com/werwe/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/werwe/android/sunshine/app/MainActivity.java
@@ -1,10 +1,14 @@
 package com.werwe.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,9 +44,29 @@ public class MainActivity extends ActionBarActivity {
             startActivity(intent);
             return true;
         }
+        else if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void openPreferredLocationInMap() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String location = preferences.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+        Uri geoLoction = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
 
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLoction);
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+        else
+            Toast.makeText(getApplicationContext(), "Coudn't call " + location, Toast.LENGTH_SHORT).show();
+    }
 }
